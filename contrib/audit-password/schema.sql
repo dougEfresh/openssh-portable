a3ad63c03f2108b1bf34648e3e37daaa3e9d6e68
Doug Chimento <dchimento@outbrain.com>
2017-02-24 16:51:43 -0500

    Removing docker files in contrib/audit-password

--- contrib/audit-password/schema.sql
+++ contrib/audit-password/schema.sql
@@ -4,7 +4,7 @@ create database audit
 
 use audit;
 
-CREATE TABLE sshAuditPasswd (
+CREATE TABLE %%TABLE%% (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `time` bigint(20) unsigned NOT NULL,
   `user` varchar(1024) NOT NULL DEFAULT '',
@@ -15,27 +15,3 @@ CREATE TABLE sshAuditPasswd (
   `remoteVersion` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) CHARACTER SET utf8 COLLATE utf8_general_ci;
-
-
-CREATE TABLE sshAuditPasswdGeo (
-  `id` bigint NOT NULL,
-  `longitude`  DECIMAL(5,2)  NOT NULL DEFAULT 0,
-  `latitude`  DECIMAL(5,2) NOT NULL DEFAULT 0,
-  `country` char(2) NOT NULL DEFAULT 'ZZ',
-  `city` varchar(128),
-  `postal` varchar(16),
-  `metroCode` bigint(20) NOT NULL DEFAULT 0,
-   FOREIGN KEY (id)
-   REFERENCES sshAuditPasswd(id)
-) CHARACTER SET utf8 COLLATE utf8_general_ci;
-
-CREATE OR REPLACE VIEW sshEvent AS
-select a.id, FROM_UNIXTIME(time/1000) as time, user, passwd, remoteAddr, remotePort, remoteName, remoteVersion,
-COALESCE(b.latitude, 0.0) as latitude,
-COALESCE(b.longitude,0.0) as longitude,
-COALESCE(b.country, 'ZY') as country,
-COALESCE(b.city, '') as city, 
-COALESCE(b.postal, '') as postal,
-COALESCE(b.metroCode, 0) as metroCode
-FROM sshAuditPasswd a left join sshAuditPasswdGeo b on
-a.id = b.id;
--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,3 @@
 rm -rf build
 mkdir build
-docker run -ti -v $PWD/build:/opt/ssh -v $PWD:/root/build dougefresh/ubuntu-build  bash -c 'autoconf -f && autoheader -f && ./configure --disable-suid-ssh --without-rsh --with-privsep-user=nobody --prefix=/opt/ssh --with-audit-passwd-url=yes && make clean && make -j 4 && make install'
+docker run -ti -v $PWD/build:/opt/ssh -v $PWD:/root/build dougefresh/ubuntu-build  bash -c 'make distclean && autoconf -f && autoheader -f && ./configure --disable-suid-ssh --without-rsh --with-privsep-user=nobody --prefix=/opt/ssh --with-audit-passwd-url=yes && make clean && make -j 4 && make install'
