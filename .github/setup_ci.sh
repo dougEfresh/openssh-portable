d7cbca8c5aa1faab019de5fd1d3dc5b48f2435de
Doug Chimento <dchimento@gmail.com>
2020-10-18 22:48:48 +0300

    libcurl4-openssl-dev

--- .github/setup_ci.sh
+++ .github/setup_ci.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 sudo apt update -qq
-sudo apt install -qy  heimdal-dev libedit-dev libpam0g-dev libjson-c-dev libcurl-dev
+sudo apt install -qy  heimdal-dev libedit-dev libpam0g-dev libjson-c-dev  libcurl4-openssl-dev 
 
 
